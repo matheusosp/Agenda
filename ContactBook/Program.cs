94a7a4e3df69677740b8b684f80722811f0a5785
Matheus <matheusp-2014@outlook.com.br>
2023-02-24 12:48:26 -0300

    alterando local do Readme

---
+++ ContactBook/Program.cs
@@ -0,0 +1,112 @@
+﻿using System.Globalization;
+using ContactBook.Resources;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StrategyLibrary.SortingExample;
+
+namespace ContactBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ci = new CultureInfo("pt-BR");
+            CultureInfo.DefaultThreadCurrentCulture = ci;
+            CultureInfo.DefaultThreadCurrentUICulture = ci;
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+            StartApp();
+        }
+
+        private static void StartApp()
+        {
+            ChangeLanguage();
+            var agenda = new Agenda();
+            var quit = false;
+
+            do
+            {
+                Console.WriteLine(Language.SelectTheAction);
+                Console.WriteLine("1 - " + Language.AddPersons);
+                Console.WriteLine("2 - " + Language.RemovePersons);
+                Console.WriteLine("3 - " + Language.ListPersons);
+                Console.WriteLine("4 - " + Language.UpdateLanguageString);
+                Console.WriteLine("5 - " + Language.CloseApplication);
+                var action = Console.ReadLine();
+                switch (action)
+                {
+                    case "1":
+                        AddPersons(agenda);
+                        break;
+                    case "2":
+                        RemovePersons(agenda);
+                        break;
+                    case "3":
+                        Console.WriteLine(Language.StarList);
+                        agenda.ListPersons();
+                        break;
+                    case "4":
+                        ChangeLanguage();
+                        break;
+                    case "5":
+                        quit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opção invalida, digite a opção novamente");
+                        break;
+                }
+            } while (quit == false);
+        }
+        
+
+        private static void ChangeLanguage()
+        {
+            Console.WriteLine(Language.SelectLanguage);
+            var option = Console.ReadLine();
+            var ci = new CultureInfo("pt-BR");
+            if (option == "2")
+                ci = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+
+        private static void RemovePersons(Agenda agenda)
+        {
+            Console.WriteLine("Quantas pessoas deseja remover da lista?");
+            var qtd = int.Parse(Console.ReadLine());
+            if (qtd > agenda.QtdPersons)
+            {
+                while (qtd > agenda.QtdPersons)
+                {
+                    Console.WriteLine("A quantidade de pessoas a remover é maior do que a quantidade de pessoas registrada, digite outra quantidade");
+                    qtd = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < qtd; i++)
+            {
+                Console.WriteLine("Digite o id da pessoa: ");
+                var id = Console.ReadLine();
+                agenda.RemovePerson(int.Parse(id));
+            }
+        }
+
+        private static void AddPersons(Agenda agenda)
+        {
+           Console.WriteLine("Quantas pessoas deseja adicionar na lista?");
+           var qtd = int.Parse(Console.ReadLine());
+           for (int i = 0; i < qtd; i++)
+           {
+               Console.WriteLine("Digite o nome da pessoa "+ (i + 1) +": ");
+               var name = Console.ReadLine();
+               Console.WriteLine("Digite o endereço da pessoa:");
+               var endereco = Console.ReadLine();
+               Console.WriteLine("Digite o email da pessoa: ");
+               var email = Console.ReadLine();
+               var person = new Person(name,endereco,email);
+               agenda.AddPerson(person);
+           }
+        }
+        
+    }
+}
---
+++ ContactBook/Resources/Language.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Resources;
+using System.Resources.NetStandard;
+using System.Text;
+
+
+namespace ContactBook.Resources
+{
+    public static class Language
+    {
+        private static ResourceSet? _resource;
+        private static CultureInfo? _culture;
+
+        private static string? GetString(string key)
+        {
+            if (CultureInfo.CurrentCulture == _culture) return _resource.GetString(key);
+            
+            _culture = CultureInfo.CurrentCulture;
+            var reader = new ResXResourceReader($"{AppDomain.CurrentDomain.BaseDirectory}" +
+                                                $"\\Resources\\Strings.{_culture.Name}.resx");
+            _resource = new ResourceSet(reader);
+
+            return _resource.GetString(key);
+        }
+        
+        public static string? SelectLanguage
+        {
+            get { return GetString("SelectLanguage"); }
+        }
+        public static string? SelectTheAction
+        {
+            get { return GetString("SelectTheAction"); }
+        }
+        public static string? AddPersons
+        {
+            get { return GetString("AddPersons"); }
+        }
+        public static string? RemovePersons
+        {
+            get { return GetString("RemovePersons"); }
+        }
+        public static string? ListPersons
+        {
+            get { return GetString("ListPersons"); }
+        }
+        public static string? UpdateLanguageString
+        {
+            get { return GetString("UpdateLanguageString"); }
+        }
+        public static string? CloseApplication
+        {
+            get { return GetString("CloseApplication"); }
+        }
+        public static string? StarList
+        {
+            get { return GetString("StarList"); }
+        }
+    }
+}
